78b663033769f2ce51b4b4986e5ff74d5877a3c0
David Silva Rudillo <dsilva@bcn.cl>
2025-04-30 17:26:48 -0400

    Desarrollo Home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from './modules/home/home.component';
+import {DashboardComponent} from './modules/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+  path: 'home',
+  component: HomeComponent
+},{
+  path: '',
+  redirectTo: '/home',
+  pathMatch: 'full'
+}];
